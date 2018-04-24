2ef9c7e8e6f5966e61f989bc07d1c2cc3218f68f
Alf Nilsson <mail@alfnilsson.se>
2018-04-24 23:45:58 +0200

    Correct files

---
+++ ContentApi/CustomPropertyModels/PropertyModels/ImageVaultMediaReferencePropertyModel.cs
@@ -0,0 +1,14 @@
+﻿using EPiServer.ContentApi.Core;
+using ImageVault.EPiServer;
+
+namespace Toders.ContentApi.Site.ContentDelivery.PropertyModels
+{
+    public class ImageVaultMediaReferencePropertyModel : PropertyModel<MediaReference, PropertyMedia>
+    {
+        public ImageVaultMediaReferencePropertyModel(PropertyMedia propertyMedia)
+            : base(propertyMedia)
+        {
+            this.Value = propertyMedia.MediaReference;
+        }
+    }
+}
